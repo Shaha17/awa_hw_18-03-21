7ffacf4624bee26055696f8a6825dbf134a723d3
ShahaKB <shahzodjabbarov@gmail.com>
2021-03-22 21:50:42 +0500

    add sql script to create table

---
+++ DZ_18-03-21/script.sql
@@ -0,0 +1,20 @@
+USE [AlifAcademy]
+GO
+/****** Object:  Table [dbo].[Customers]    Script Date: 22.03.2021 21:49:03 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Customers](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [nvarchar](50) NOT NULL,
+	[LastName] [nvarchar](50) NOT NULL,
+	[MiddleName] [nvarchar](50) NOT NULL,
+	[DateOfBirth] [date] NOT NULL,
+ CONSTRAINT [PK_Customers] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
